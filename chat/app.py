2a6f5ccc00a84b6f28b3649acbf52fafb848c3e7
leolinf <funlin.py@foxmail.com>
2018-02-08 15:26:15 +0800

    update chat

--- chat/app.py
+++ chat/app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 
 
 app = Flask(__name__)
@@ -8,30 +9,46 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['DEBUG'] = True
 socketio=SocketIO(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @socketio.on('my event', namespace='/test')
 def test_message(message):
     emit('my response', {'data': message['data']})
     print('test_message my event')
 
+
 @socketio.on('my broadcast event', namespace='/test')
 def test_message(message):
+    # 未命名事件
     emit('my response', {'data': message['data']}, broadcast=True)
     print('test_message mybraodcast')
+
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
     emit('my response', {'data': 'Connected'})
+    print("test connect")
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
 
+
+@socketio.on('json')
+def hander_json(json):
+    print('receied json: %s', str(json))
+
+
 @socketio.on('message')
 def handler_message(message):
+    # 命名事件
+
+    send(message)
     print('recevied message:'+ str(message))
 
 
