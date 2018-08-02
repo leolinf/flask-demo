7a1dc421ec66c29b229371264d622c8031ffba8d
leolinf <funlin.py@foxmail.com>
2018-08-02 15:51:54 +0800

    rm docker

--- risk/publish.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-docker build -t hub.cdecube.com/riskctrl/riskctrlpy:2.1 .
-docker push hub.cdecube.com/riskctrl/riskctrlpy:2.1
--- risk/start_celery.sh
+++
@@ -1,2 +0,0 @@
-service filebeat start
-su user -c 'celery -A celery_app.celery worker -B -f /var/log/riskctrlpy_celery.log -Q unlock_q,monitor_q,sms_q,anti_q -c 2 -s /tmp/xxx.beat'
